c4ef071586fc86cc7acceb4262f9e322f5d7c2d8
italoangelo13 <italoangelo.ti@gmail.com>
2017-05-19 09:42:46 -0300

    Cabeçalho e Rodapé criados

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,42 +1,31 @@
-<!doctype html>
-<html>
-    <head>
-        <title>Central de Informatica e Idiomas - Cursos de Informatica, Idiomas e Profissionalizantes</title>
-        <meta charset="UTF-8" />
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-    </head>
-    <body>
-        <div class="container-fluid">
-            <header class="row">
-                <nav class="navbar navbar-toggleable-md navbar-light bg-faded col-lg-12">
-                    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <a class="navbar-brand">Menu</a>
+<?php include_once 'header.php'; ?>
 
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Institucional</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Cursos</a>
-                            </li>
-                            
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Area do Aluno</a>
-                            </li>
-                        </ul>
-                        <form class="form-inline my-2 my-lg-0">
-                            <input class="form-control mr-sm-2" type="text" placeholder="O Que você Procura?">
-                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
-                        </form>
+<div class="row">
+                <div class="jumbotron jumbotron-fluid col-lg-12" style="background: url('http://www.cditelecom.com.br/wp-content/uploads/2015/04/banner1.jpg') center;">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <img src="" alt="" title="" />
+                            </div>
+                            <div class="col-lg-8" style="color: #fff;">
+                                <center><h1 class="display-4" style="font-weight: 500;">Central de Informatica e Idiomas</h1>
+                                    <p class="lead">Qualidade de ensino para Você alcançar o sucesso!</p></center>
+                            </div>
+                        </div>
+                        
+                        <div class="row">
+                            Redes Sociais
+                        </div>
                     </div>
-                </nav>
-            </header>
+                </div>
+            </div>
         </div>
-    </body>
-</html>
+
+<?php include_once 'footer.php'; ?>
+
+
+
+
+
+
+        

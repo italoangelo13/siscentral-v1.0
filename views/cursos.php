db8593701e50dc73445304f5304b7515bc67df28
italoangelo13 <italoangelo.ti@gmail.com>
2017-06-09 11:10:59 -0300

    Curso pronto

--- views/cursos.php
+++ views/cursos.php
@@ -21,7 +21,25 @@ if(!$resultado){ ?>
     </h1>
 </div>
 
-<div class="row">
-    
+<div class="row" style="padding: 10px;">
+    <?php
+    while ($dados = $resultado -> fetch_assoc()){
+        $nome = $dados['nome'];
+        $sumario = $dados['sumario'];
+        $carga = $dados['carga'];
+        $duracao = $dados['duracao'];
+        $img = $dados['imagem'];
+    ?>
+    <div class="card col-lg-5" style=" padding-top: 5px; margin-left: 50px; margin-right: 50px; margin-bottom: 20px;">
+        <img class="card-img-top" src="../assets/img/img-cursos/<?php echo $img; ?>" alt="<?php echo $nome; ?>" title="<?php echo $nome; ?>" width="100%">
+    <div class="card-block">
+        <h4 class="card-title"><?php echo $nome; ?> <small>Carga Horaria: <?php echo $carga; ?> Horas - (<?php echo $duracao; ?>)</small></h4>
+    <p class="card-text" ><?php echo $sumario; ?></p>
+    <a href="#" class="btn btn-primary">Ver Mais</a>
+  </div>
+</div>
+    <?php
+    }
+    ?>
 </div>
 <?php include_once 'footer.php'; ?>
